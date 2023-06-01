6039eb8cf68bbfd206d5accd8a3a4595a2113797
Grknaky16 <grkn.aky@gmail.com>
2023-06-01 13:57:33 +0300

    V3

--- Assets/Scripts/AdScript.cs
+++ Assets/Scripts/AdScript.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using GoogleMobileAds.Api;
@@ -8,97 +6,119 @@ using UnityEngine.SceneManagement;
 
 public class AdScript : MonoBehaviour
 {
-    //public static adscript ýnstance;
-    //private ýnterstitialad interstitial_ad;
-    //private rewardedad rewardedad;
-    //public bool endgame = false;
+    public static AdScript Instance;
+    RewardedAd rewardedAd;
+    public string _adUnitId;
+    public int puan;
+    public int coin;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            LoadRewardedAd();
+        });
+    }
 
-    //public int puan = 0;
-    //public int coin = 0;
+    public void LoadRewardedAd()
+    {
+        // Clean up the old ad before loading a new one.
+        if (rewardedAd != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+            rewardedAd.Destroy();
+            rewardedAd = null;
+        }
 
-    //private string interstitial_ad_ýd;
-    //private string rewardedad_ýd;
+        Debug.Log("Loading the rewarded ad.");
 
-    //void start()
-    //{
-    //    interstitial_ad_ýd = "ca-app-pub-3940256099942544/1033173712";
-    //    rewardedad_ýd = "ca-app-pub-3940256099942544/5224354917";
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
 
-    //    mobileads.ýnitialize(initstatus => { });
+        // send the request to load the ad.
+        RewardedAd.Load(_adUnitId, adRequest,
+            (RewardedAd ad, LoadAdError error) =>
+            {
+                // if error is not null, the load request failed.
+                if (error != null || ad == null)
+                {
+                    Debug.LogError("Rewarded ad failed to load an ad " +
+                                   "with error : " + error.GetMessage());
+                    return;
+                }
 
-    //    requestýnterstitial();
-    //    requestrewardedvideo();
-    //}
+                Debug.Log("Rewarded ad loaded with response : "
+                          + ad.GetResponseInfo());
 
-    //private void requestýnterstitial()
-    //{
-    //    interstitial_ad = new ýnterstitialad(interstitial_ad_ýd);
-    //    interstitial_ad.onadloaded += handleonanloaded;
-    //    adrequest request = new adrequest.builder().build();
-    //    interstitial_ad.loadad(request);
-    //}
-
-    //private void requestrewardedvideo()
-    //{
-    //    rewardedad = new rewardedad(rewardedad_ýd);
-    //    rewardedad.onuserearnedreward += handleuserearnedreward;
-    //    rewardedad.onadclosed += handlerewardedadclosed;
-    //    rewardedad.onadfailedtoshow += handlerewardedadfailedtoshow;
-    //    adrequest request = new adrequest.builder().build();
-    //    rewardedad.loadad(request);
-    //}
-
-    //public void showýnterstitial()
-    //{
-    //    if (interstitial_ad.ýsloaded())
-    //    {
-    //        interstitial_ad.show();
-    //        requestýnterstitial();
-
-    //    }
-    //}
-
-    //public void showrewardedvideo()
-    //{
-    //    if (rewardedad.ýsloaded())
-    //    {
-    //        rewardedad.show();
-    //        if (endgame == false)
-    //        {
-    //            //scenemanager.loadscene(scenemanager.getactivescene().buildýndex + 1);
-    //            puan = playerprefs.getýnt("puan");
-    //            coin = playerprefs.getýnt("catchtheball");
-    //            puan += coin;
-    //            playerprefs.deletekey("catchtheball");
-    //            playerprefs.setýnt("puan", puan);
-    //        }
-    //        else
-    //        {
-    //            scenemanager.loadscene(scenemanager.getactivescene().buildýndex);
-    //        }
-
-    //    }
-    //}
-
-    //public void handleonanloaded(object sender, eventargs args)
-    //{
-
-    //}
-    //public void handlerewardedadfailedtoshow(object sender, eventargs args)
-    //{
-    //    requestrewardedvideo();
-    //}
-
-    //public void handlerewardedadclosed(object sender, eventargs args)
-    //{
-    //    scenemanager.loadscene(scenemanager.getactivescene().buildýndex + 1);
-    //}
-
-    //public void handleuserearnedreward(object sender, eventargs args)
-    //{
-    //    requestrewardedvideo();
-    //}
+                rewardedAd = ad;
+                RegisterEventHandlers(rewardedAd);
+            });
+    }
 
 
+    public void ShowRewardedAd()
+    {
+        
 
+        if (rewardedAd != null && rewardedAd.CanShowAd())
+        {
+            rewardedAd.Show((Reward reward) =>
+            {
+                // TODO: Reward the user.
+                puan = PlayerPrefs.GetInt("Puan");
+                coin = PlayerPrefs.GetInt("catchtheball");
+                puan += coin;
+                PlayerPrefs.DeleteKey("catchtheball");
+                PlayerPrefs.SetInt("Puan", puan);
+
+            });
+        }
+    }
+
+
+    private void RegisterEventHandlers(RewardedAd ad)
+    {
+        // Raised when the ad is estimated to have earned money.
+        ad.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log(String.Format("Rewarded ad paid {0} {1}.",
+                adValue.Value,
+                adValue.CurrencyCode));
+        };
+        // Raised when an impression is recorded for an ad.
+        ad.OnAdImpressionRecorded += () =>
+        {
+            Debug.Log("Rewarded ad recorded an impression.");
+        };
+        // Raised when a click is recorded for an ad.
+        ad.OnAdClicked += () =>
+        {
+            Debug.Log("Rewarded ad was clicked.");
+        };
+        // Raised when an ad opened full screen content.
+        ad.OnAdFullScreenContentOpened += () =>
+        {
+            Debug.Log("Rewarded ad full screen content opened.");
+        };
+        // Raised when the ad closed full screen content.
+        ad.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Rewarded ad full screen content closed.");
+            LoadRewardedAd();
+        };
+        // Raised when the ad failed to open full screen content.
+        ad.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Rewarded ad failed to open full screen content " +
+                           "with error : " + error);
+            LoadRewardedAd();
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
--- Assets/Scripts/RestartAdScript.cs
+++ Assets/Scripts/RestartAdScript.cs
@@ -3,90 +3,120 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using GoogleMobileAds;
 using GoogleMobileAds.Api;
 using UnityEngine.SceneManagement;
 
 public class RestartAdScript : MonoBehaviour
 {
-    //public static RestartAdScript ýnstance;
-    //private InterstitialAd interstitial_ad;
-    //private RewardedAd rewardedad;
-    //public bool endgame = false;
+    public static RestartAdScript Instance;
+    RewardedAd rewardedAd;
+    public string _adUnitId;
 
-    //public int puan = 0;
-    //public int coin = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            LoadRewardedAd();
+        });
+    }
 
-    //private string interstitial_ad_ýd;
-    //private string rewardedad_ýd;
+    public void LoadRewardedAd()
+    {
+        // Clean up the old ad before loading a new one.
+        if (rewardedAd != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            rewardedAd.Destroy();
+            rewardedAd = null;
+        }
 
-    //void start()
-    //{
-    //    interstitial_ad_ýd = "ca-app-pub-3940256099942544/1033173712";
-    //    rewardedad_ýd = "ca-app-pub-3940256099942544/5224354917";
+        Debug.Log("Loading the rewarded ad.");
 
-    //    MobileAds.Initialize(initstatus => { });
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
 
-    //    requestýnterstitial();
-    //    requestrewardedvideo();
-    //}
+        // send the request to load the ad.
+        RewardedAd.Load(_adUnitId, adRequest,
+            (RewardedAd ad, LoadAdError error) =>
+            {
+                // if error is not null, the load request failed.
+                if (error != null || ad == null)
+                {
+                    Debug.LogError("Rewarded ad failed to load an ad " +
+                                   "with error : " + error.GetMessage());
+                    return;
+                }
 
-    //private void requestýnterstitial()
-    //{
-    //    interstitial_ad = new InterstitialAd(interstitial_ad_ýd);
-    //    interstitial_ad.OnAdLoaded += handleonanloaded;
-    //    adrequest request = new adrequest.builder().build();
-    //    interstitial_ad.loadad(request);
-    //}
+                Debug.Log("Rewarded ad loaded with response : "
+                          + ad.GetResponseInfo());
 
-    //private void requestrewardedvideo()
-    //{
-    //    rewardedad = new rewardedad(rewardedad_ýd);
-    //    rewardedad.onuserearnedreward += handleuserearnedreward;
-    //    rewardedad.onadclosed += handlerewardedadclosed;
-    //    rewardedad.onadfailedtoshow += handlerewardedadfailedtoshow;
-    //    adrequest request = new adrequest.builder().build();
-    //    rewardedad.loadad(request);
-    //}
+                rewardedAd = ad;
+                RegisterEventHandlers(rewardedAd);
+            });
+    }
 
-    //public void showýnterstitial()
-    //{
-    //    if (interstitial_ad.ýsloaded())
-    //    {
-    //        interstitial_ad.show();
-    //        requestýnterstitial();
 
-    //    }
-    //}
+    public void ShowRewardedAd()
+    {
 
-    //public void showrewardedvideo()
-    //{
-    //    if (rewardedad.ýsloaded())
-    //    {
-    //        rewardedad.show();
 
-    //    }
-    //}
+        if (rewardedAd != null && rewardedAd.CanShowAd())
+        {
+            rewardedAd.Show((Reward reward) =>
+            {
+                // TODO: Reward the user.
+               
 
-    //public void handleonanloaded(object sender, eventargs args)
-    //{
+            });
+        }
+    }
 
-    //}
-    //public void handlerewardedadfailedtoshow(object sender, eventargs args)
-    //{
-    //    requestrewardedvideo();
-    //}
 
-    //public void handlerewardedadclosed(object sender, eventargs args)
-    //{
-    //    scenemanager.loadscene(scenemanager.getactivescene().buildýndex);
-    //}
+    private void RegisterEventHandlers(RewardedAd ad)
+    {
+        // Raised when the ad is estimated to have earned money.
+        ad.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log(String.Format("Rewarded ad paid {0} {1}.",
+                adValue.Value,
+                adValue.CurrencyCode));
+        };
+        // Raised when an impression is recorded for an ad.
+        ad.OnAdImpressionRecorded += () =>
+        {
+            Debug.Log("Rewarded ad recorded an impression.");
+        };
+        // Raised when a click is recorded for an ad.
+        ad.OnAdClicked += () =>
+        {
+            Debug.Log("Rewarded ad was clicked.");
+        };
+        // Raised when an ad opened full screen content.
+        ad.OnAdFullScreenContentOpened += () =>
+        {
+            Debug.Log("Rewarded ad full screen content opened.");
+        };
+        // Raised when the ad closed full screen content.
+        ad.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Rewarded ad full screen content closed.");
+            LoadRewardedAd();
+        };
+        // Raised when the ad failed to open full screen content.
+        ad.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Rewarded ad failed to open full screen content " +
+                           "with error : " + error);
+            LoadRewardedAd();
+        };
+    }
 
-    //public void handleuserearnedreward(object sender, eventargs args)
-    //{
-    //    requestrewardedvideo();
-    //}
+    // Update is called once per frame
+    void Update()
+    {
 
+    }
 
 
 }
